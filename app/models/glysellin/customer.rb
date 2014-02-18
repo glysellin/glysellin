4ef81921ac41fcc945987352055ce7c9835fa37d
zenati <yassine.zenati@wifirst.fr>
2014-02-18 18:24:22 +0100

    Added: full_name method

--- app/models/glysellin/customer.rb
+++ app/models/glysellin/customer.rb
@@ -3,5 +3,9 @@ module Glysellin
     self.table_name = 'glysellin_customers'
 
     validates_presence_of :first_name, :last_name, :email
+
+    def full_name
+      [first_name, last_name].join(' ')
+    end
   end
 end
