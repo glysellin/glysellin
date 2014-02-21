af42d525b9de28e861e95816cd125cd56c1dae95
zenati <yassine.zenati@wifirst.fr>
2014-02-21 15:49:29 +0100

    Added: locale in errors

--- app/models/glysellin/sellable.rb
+++ app/models/glysellin/sellable.rb
@@ -18,7 +18,7 @@ module Glysellin
     has_many :variants, class_name: "Glysellin::Variant", inverse_of: :sellable, dependent: :destroy
     accepts_nested_attributes_for :variants, allow_destroy: true, reject_if: :all_blank
 
-    validates :variants, length: { minimum: 1, too_short: "Merci de renseigner %{count} variante au minimum." }
+    validates :variants, length: { minimum: 1, too_short: I18n.t("glysellin.errors.variants.too_short") }
 
     # Published sellables are the ones that have at least one variant
     # published.
