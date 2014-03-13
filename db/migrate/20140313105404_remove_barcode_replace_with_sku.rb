7af8d3aef685422c20cdfbea3adf65d6b50641ad
zenati <yassine.zenati@wifirst.fr>
2014-03-13 12:08:08 +0100

    Added: up down

--- db/migrate/20140313105404_remove_barcode_replace_with_sku.rb
+++ db/migrate/20140313105404_remove_barcode_replace_with_sku.rb
@@ -1,5 +1,9 @@
 class RemoveBarcodeReplaceWithSku < ActiveRecord::Migration
-  def change
+  def up
     remove_column :glysellin_variants, :barcode
+  end
+
+  def down
+    add_column :glysellin_variants, :barcode, :string
   end
 end
