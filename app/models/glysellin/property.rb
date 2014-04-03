f686eff068f2c030f376e5459f0e1686e926e1e8
ZENATI YASSINE <zenati@informatique.univ-paris-diderot.fr>
2014-04-03 15:11:09 +0200

    Added: sellables property

--- app/models/glysellin/property.rb
+++ app/models/glysellin/property.rb
@@ -4,6 +4,7 @@ module Glysellin
 
     has_many :variant_properties, class_name: 'Glysellin::VariantProperty', dependent: :destroy, inverse_of: :variant
     has_many :variants, class_name: 'Glysellin::Variant', through: :variant_properties
+    has_many :sellables, through: :variants
     belongs_to :property_type, class_name: 'Glysellin::PropertyType'
 
     validates :value, :barcode_ref, presence: true
