ec2a93def3b048b4d11ca368e0233a0c2815ca4a
ZENATI YASSINE <zenati@informatique.univ-paris-diderot.fr>
2014-04-03 13:44:43 +0200

    Fixed: migrations

--- db/migrate/20140218164618_rename_user_id_to_customer_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserIdToCustomerId < ActiveRecord::Migration
-  def change
-    rename_column :glysellin_orders, :user_id, :customer_id
-  end
-end
--- db/migrate/20140220093342_fix_email_definition_in_customers.rb
+++ db/migrate/20140220093342_fix_email_definition_in_customers.rb
@@ -1,6 +1,5 @@
 class FixEmailDefinitionInCustomers < ActiveRecord::Migration
   def change
-    remove_column :glysellin_customers, :email
     add_column :glysellin_customers, :email, :string
   end
 end
