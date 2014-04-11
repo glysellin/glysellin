f0bcaaa3b16a0b752001908fa6fd454a28121e51
ZENATI YASSINE <zenati@informatique.univ-paris-diderot.fr>
2014-04-11 18:19:04 +0200

    Udpated: taxonomy

--- app/models/glysellin/taxonomy.rb
+++ app/models/glysellin/taxonomy.rb
@@ -4,7 +4,7 @@ module Glysellin
 
     scope :roots, -> { where(parent_id: nil) }
 
-    has_many :products, class_name: 'Glysellin::Sellable', dependent: :nullify
+    has_many :sellables, dependent: :nullify
     has_many :children, class_name: 'Glysellin::Taxonomy', foreign_key: 'parent_id', dependent: :destroy
     belongs_to :parent, class_name: 'Glysellin::Taxonomy'
 
@@ -12,7 +12,7 @@ module Glysellin
     validates :barcode_ref, presence: true
 
     def self.order_and_print
-      self.includes(:products).order('name asc').map { |b| ["#{b.name} (#{b.products.size} produit(s))", b.id] }
+      self.includes(:sellables).order('name asc').map { |b| ["#{b.name} (#{b.sellables.size} produit(s))", b.id] }
     end
 
     def path
