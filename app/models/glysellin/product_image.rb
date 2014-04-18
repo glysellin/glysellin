06d2d7569d932eecd08bd364684be6b06e0d1936
ZENATI YASSINE <zenati@informatique.univ-paris-diderot.fr>
2014-04-18 17:01:34 +0200

    Removed: not needed model

--- app/models/glysellin/product_image.rb
+++
@@ -1,9 +0,0 @@
-module Glysellin
-  class ProductImage < ActiveRecord::Base
-    self.table_name = 'glysellin_product_images'
-
-    belongs_to :imageable, :polymorphic => true
-
-    has_attached_file :image, :styles => Glysellin.product_images_styles
-  end
-end
