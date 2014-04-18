0cbacc4a8c9faa787819876759d4bd28b52162d9
ZENATI YASSINE <zenati@informatique.univ-paris-diderot.fr>
2014-04-18 16:18:47 +0200

    Udpated: serializer

--- app/serializers/glysellin/taxonomy_serializer.rb
+++ app/serializers/glysellin/taxonomy_serializer.rb
@@ -1,8 +1,7 @@
 module Glysellin
   class TaxonomySerializer < ActiveModel::Serializer
-    embed :ids, include: true
-    attributes :id, :name, :barcode_ref, :parent_id, :deep_sellables_count, :deep_children_ids
-    has_many :children
-    has_many :sellables
+    embed :ids
+    attributes :id, :name, :barcode_ref, :parent_id
+    has_many :children, include: true, serializer: ShallowTaxonomySerializer
   end
 end
