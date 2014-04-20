55c9775963e8f8624c478b2abb10effe2387bd45
ZENATI YASSINE <zenati@informatique.univ-paris-diderot.fr>
2014-04-20 13:41:46 +0200

    Added: sellables without variants

---
+++ app/serializers/glysellin/sellable_without_variant_serializer.rb
@@ -0,0 +1,8 @@
+module Glysellin
+  class SellableWithoutVariantSerializer < ActiveModel::Serializer
+    embed :ids, include: true
+
+    attributes :id, :name, :description, :vat_rate, :eot_price, :price, :weight,
+      :unlimited_stock, :barcode_ref, :variant_ids
+  end
+end
