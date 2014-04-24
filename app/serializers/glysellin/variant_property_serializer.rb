883e4c94f68e27ed0113577a0a28c7b8b55146c8
User <user@MacBook-Alex.local>
2014-04-24 18:03:17 +0200

    Removed:

--- app/serializers/glysellin/variant_property_serializer.rb
+++ app/serializers/glysellin/variant_property_serializer.rb
@@ -1,10 +1,7 @@
 module Glysellin
   class VariantPropertySerializer < ActiveModel::Serializer
     embed :ids
-
     attributes :id
-
-    has_one :variant
     has_one :property, include: true
   end
 end
