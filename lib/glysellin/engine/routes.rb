ad59c3f86280de543a134e73394cb533326f9769
User <user@MacBook-Alex.local>
2014-04-25 18:26:17 +0200

    Removed: routes

--- lib/glysellin/engine/routes.rb
+++ lib/glysellin/engine/routes.rb
@@ -36,16 +36,9 @@ module ActionDispatch::Routing
 
         scope module: 'glysellin' do
           namespace :api do
-            resource :store, controller: 'store'
-            resources :sellables, only: [:index, :show]
-            resources :variants, only: :index
             resources :properties, only: :index
             resources :property_types, only: :index
             resources :orders, only: :create
-            resources :menus, only: :index
-            resources :taxonomies, only: [:index, :show] do
-              resources :sellables
-            end
           end
         end
         # get '/' => 'glysellin/products#index', as: 'shop'
