aad1bc856f1a006162f2588b43fa76dedbe1bfb2
vala <gonjo@free.fr>
2014-07-22 12:35:26 +0200

    clean up addresses filling checkout step

--- app/helpers/glysellin/orders_helper.rb
+++ app/helpers/glysellin/orders_helper.rb
@@ -8,25 +8,20 @@ module Glysellin
     #
     def addresses_fields_for form, record = nil
       # Copy addresses from record to form object
-      # if record
-      #   %w(billing_address shipping_address).each do |addr|
-      #     next if (address = form.object.send(addr).presence) && address.id.present?
-      #       form.object.send("build_#{ addr }", attrs)
 
-      #     if (address = record.send("#{ addr }"))
-      #       attrs = address.attributes.select do |key, value|
-      #         Glysellin::Address.accessible_attributes.include?(key)
-      #       end
-      #     end
-      #   end
+      if record && record.use_another_address_for_shipping && !form.object.billing_address
+        form.object.use_another_address_for_shipping = true
+      end
 
-      #   unless form.object.use_another_address_for_shipping.present?
-      #     form.object.use_another_address_for_shipping = record.use_another_address_for_shipping
-      #   end
-      # end
+      %w(billing_address shipping_address).each do |addr|
+        next if form.object.send(addr)
 
-      form.object.build_billing_address  unless record.billing_address
-      form.object.build_shipping_address unless record.shipping_address
+        if record && (address = record.send("#{ addr }"))
+          form.object.send(:"#{ addr }=", address)
+        else
+          form.object.send(:"build_#{ addr }")
+        end
+      end
 
       render partial: 'glysellin/orders/addresses_fields', locals: { form: form }
     end
