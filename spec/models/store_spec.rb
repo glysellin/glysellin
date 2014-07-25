4ee2a3da0e7935f79712cb844f5a3c66ab8f91f0
zenati <zenati@informatique.univ-paris-diderot.fr>
2014-07-25 14:17:35 +0200

    adds store_spec

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -3,4 +3,43 @@ require "spec_helper"
 describe Glysellin::Store do
   it { should have_many(:stocks) }
   it { should have_many(:variants) }
+  it { should have_many(:orders) }
+
+  it { should validate_presence_of(:name) }
+
+  let(:store) { create(:store) }
+
+  describe '#in_stock?' do
+    it 'returns true if sellable is unlimited' do
+      sellable = create(:sellable, unlimited_stock: true)
+      variant = sellable.variants.sample
+      variant.stores << store
+
+      expect(store.in_stock?(variant)).to eq true
+    end
+
+    it 'returns true if sellable is not unlimited but available' do
+      sellable = create(:sellable, unlimited_stock: false)
+      variant = sellable.variants.sample
+      variant.stores << store
+
+      stock = variant.stocks.where(store_id: store.id).first
+      stock.count = 10
+      stock.save!
+
+      expect(store.in_stock?(variant)).to eq true
+    end
+
+    it 'returns false if sellable is not unlimited but available' do
+      sellable = create(:sellable, unlimited_stock: false)
+      variant = sellable.variants.sample
+      variant.stores << store
+
+      stock = variant.stocks.where(store_id: store.id).first
+      stock.count = 0
+      stock.save!
+
+      expect(store.in_stock?(variant)).to eq false
+    end
+  end
 end
