1861d4efe1996db27671815ceceba3e87aec3d5b
zenati <zenati@informatique.univ-paris-diderot.fr>
2014-07-28 12:34:24 +0200

    adds more order_specs

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,115 +1,97 @@
 require "spec_helper"
 
 describe Glysellin::Order do
-  it { should have_many(:products) }
-  it { should belong_to(:customer) }
   it { should have_many(:payments) }
-  it { should belong_to(:shipping_method) }
-  it { should have_many(:order_adjustments) }
+  it { should have_one(:cart) }
+  it { should have_one(:invoice) }
 
-  it { should accept_nested_attributes_for(:products) }
+  it { should belong_to(:customer) }
   it { should accept_nested_attributes_for(:payments) }
-  it { should accept_nested_attributes_for(:order_adjustments) }
 
+  it { should validate_presence_of(:customer) }
   it { should validate_presence_of(:billing_address) }
-  it { should validate_presence_of(:shipping_address) }
-  it { should validate_presence_of(:products) }
+  it { should validate_presence_of(:parcels) }
+
+  before(:each) do
+    @order = create(:order)
+  end
+
+  context 'before validation' do
+    it 'checks prices' do
+      expect(@order).to receive(:process_total_price)
+      @order.save
+    end
+  end
 
   describe "callbacks" do
-    let(:order) { build(:order) }
-
     context "before validation" do
       it "processes adjustments" do
-        expect(order).to receive(:process_adjustments)
-        order.save
-      end
-
-      it "set payment state to paid if paid by check" do
-        expect(order).to receive(:set_paid_if_paid_by_check)
-        order.save
-      end
-
-      it "notifies shipping if needed" do
-        expect(order).to receive(:notify_shipped)
-        order.save
-      end
-    end
-
-    context "after create" do
-      it "ensures an order reference is set" do
-        expect(order).to receive(:ensure_ref)
-        order.save(validate: false)
-      end
-
-      it "ensures a customer address is present" do
-        expect(order).to receive(:ensure_customer_addresses)
-        order.save(validate: false)
+        expect(@order).to receive(:process_payments)
+        @order.save
       end
     end
   end
 
   describe "#ensure_ref" do
-    let(:order) { build(:order) }
-
     it "sets the order reference to TIMESTAMP-ORDER_ID by defaut" do
-      allow(order).to receive(:id) { 1234412 }
-      order.ensure_ref
-      expect(order.ref.split(/-/).pop).to eq("1234412")
+      @order.ensure_ref
+      expect(@order.ref.split(/-/).pop).to eq('1')
+    end
+
+    it "sets the order reference to TIMESTAMP-ORDER_ID if another :id" do
+      @order.ref = '123456'
+      @order.ensure_ref
+      expect(@order.ref).to eq('123456')
     end
 
     it "uses Glysellin.order_reference_generator" do
-      # Save default generator
-      original_generator = Glysellin.order_reference_generator
-
-      Glysellin.order_reference_generator = ->(order) { "REF" }
-      order.ensure_ref
-      expect(order.ref).to eq("REF")
-
-      # Reset default generator
-      Glysellin.order_reference_generator = original_generator
+      @order.ensure_ref
+      expect(@order.ref).to eq "#{ Time.now.strftime('%Y%m%d%H%M') }-#{ @order.id }"
     end
   end
 
   describe "#ensure_customer_addresses" do
-    let(:order) { build(:order) }
+    [
+      [false, false, false, :to, :not_to     ],
+      [true, false, false,  :not_to, :not_to ],
+      [false, true, false,  :to, :not_to     ],
+      [true, true, false,   :not_to, :not_to ],
+      [false, false, true,  :to, :to         ],
+      [true, false, true,   :not_to, :to     ],
+      [false, true, true,   :to, :not_to     ],
+      [true, true, true,    :not_to, :not_to ]
+    ].each do |(billing_address, shipping_address, use_another_address_for_shipping, comparator_1, comparator_2)|
 
-    it "adds order's billing address to the customer if customer doesn't have one yet" do
-      order.customer.billing_address = nil
-      order.ensure_customer_addresses
-      expect(order.customer.billing_address.clone_attributes).to eq(
-        order.billing_address.clone_attributes
-      )
-    end
+      it "fills addresses with following arguments: {
+        (
+          #{billing_address},
+          #{shipping_address},
+          #{use_another_address_for_shipping},
+          #{comparator_1},
+          #{comparator_2}
+        )
+      }" do
+        @order.customer.billing_address = billing_address ? create(:address) : nil
+        @order.customer.shipping_address = shipping_address ? create(:address) : nil
+        @order.customer.use_another_address_for_shipping = use_another_address_for_shipping
+        @order.ensure_customer_addresses
 
-    it "doesn't replace existing Customer#billing_address" do
-      order.customer.billing_address = build(:address)
-      order.ensure_customer_addresses
-      expect(order.customer.billing_address.clone_attributes).not_to eq(
-        order.billing_address.clone_attributes
-      )
-    end
+        customer_billing_address = @order.customer.billing_address
+        customer_shipping_address = @order.customer.shipping_address
 
-    it "adds order's shipping address to the customer if needed" do
-      order.customer.shipping_address = nil
-      order.ensure_customer_addresses
-      expect(order.customer.shipping_address.clone_attributes).to eq(
-        order.shipping_address.clone_attributes
-      )
-    end
+        order_billing_address = @order.billing_address
+        order_shipping_address = @order.shipping_address
 
-    it "doesn't replace existing Customer#shipping_address" do
-      order.customer.billing_address = build(:address)
-      order.customer.shipping_address = build(:address)
-      order.ensure_customer_addresses
-      expect(order.customer.shipping_address.clone_attributes).not_to eq(
-        order.shipping_address.clone_attributes
-      )
-    end
+        for attribute in [:first_name, :last_name, :address, :zip, :city, :country]
+          unless billing_address
+            expect(customer_billing_address.send(attribute)).send(comparator_1, eq(order_billing_address.send(attribute)))
 
-    it "lets shipping address blank if billing address is the same as the shipping one" do
-      order.shipping_address.assign_attributes(order.billing_address.clone_attributes)
-      order.ensure_customer_addresses
-      expect(order.customer.shipping_address).to be_nil
+            if use_another_address_for_shipping && !shipping_address
+              expect(customer_shipping_address.send(attribute)).send(comparator_2, eq(order_shipping_address.send(attribute)))
+            end
+          end
+        end
+      end
     end
   end
 end
