33719f928da9c171c1bdc26383ffdef8dfb1b99c
zenati <zenati@informatique.univ-paris-diderot.fr>
2014-07-29 13:13:12 +0200

    adds address spec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -4,48 +4,92 @@ describe Glysellin::Address do
   it { should belong_to(:shipped_addressable) }
   it { should belong_to(:billed_addressable) }
 
-  it "validates all keys configured to be required" do
-    Glysellin.address_presence_validation_keys.each do |key|
-      expect(subject).to validate_presence_of(key)
+  describe 'validations' do
+    context 'validates presence of company_name if first_name is present' do
+      before { subject.first_name = :first_name }
+      it { should_not validate_presence_of(:company_name) }
+    end
+
+    context 'validates presence of company_name if first_name is present' do
+      before { subject.first_name = nil }
+      it { should validate_presence_of(:company_name) }
+    end
+
+    context 'validates presence of first_name if company_name is present' do
+      before { subject.company_name = :company_name }
+      it { should_not validate_presence_of(:first_name) }
+      it { should_not validate_presence_of(:last_name) }
+    end
+
+    context 'validates presence of first_name if company_name is present' do
+      before { subject.company_name = nil }
+      it { should validate_presence_of(:first_name) }
+      it { should validate_presence_of(:last_name) }
     end
   end
 
-  describe "#same_as?" do
-    it "returns true if #clone_attributes of both Address objects are equal" do
-      first_address = build(:address)
-      second_address = build(:address)
-      allow(first_address).to receive(:clone_attributes) { { a: 1 } }
-      allow(second_address).to receive(:clone_attributes) { { a: 1 } }
-      expect(first_address.same_as?(second_address)).to eq(true)
-    end
+  describe '#location_string' do
+    zip = '75014'
+    city = 'Paris'
+    country = 'France'
 
-    it "returns false if #clone_attributes of both Address objects aren't equal" do
-      first_address = build(:address)
-      second_address = build(:address)
-      allow(first_address).to receive(:clone_attributes) { { a: 1 } }
-      allow(second_address).to receive(:clone_attributes) { { a: 2 } }
-      expect(first_address.same_as?(second_address)).to eq(false)
-    end
-  end
+    [
+      [true, true,    true, '75014, Paris, France'],
+      [true, true,   false, '75014, Paris'],
+      [true, false,   true, '75014, France'],
+      [true, false,  false, '75014'],
+      [false, true,   true, 'Paris, France'],
+      [false, true,  false, 'Paris'],
+      [false, false,  true, 'France'],
+      [false, false, false, '']
+    ].each do |(zip_presence, city_presence, country_presence, expected_result)|
 
-  describe "#clone_attributes" do
-    let(:address) { build(:address) }
+      it "fills the address with following arguments: {
+        (
+          #{zip_presence},
+          #{city_presence},
+          #{country_presence}
+        )
+      }" do
+        address = build(:address)
+        address.zip = zip_presence         ? zip     : nil
+        address.city = city_presence       ? city    : nil
+        address.country = country_presence ? country : nil
 
-    it "returns all the relevant model's attributes to create a similar one" do
-      [
-        "first_name", "last_name", "address", "city", "zip", "country"
-      ].each do |key|
-        expect(address.clone_attributes.keys).to include(key)
+        expect(address.location_string).to eq expected_result
       end
     end
+  end
 
-    it "doesn't return unique attributes" do
-      [
-        "id", "created_at", "updated_at",
-        "billed_addressable_id", "billed_addressable_type",
-        "shipping_addressable_id", "shipped_addressable_type"
-      ].each do |key|
-        expect(address.clone_attributes.keys).not_to include(key)
+  describe '#full_name' do
+    first_name = 'First'
+    last_name = 'Last'
+    company_name = 'Company'
+
+    [
+      [true, true,    true, 'First Last Company'],
+      [true, true,   false, 'First Last'],
+      [true, false,   true, 'First Company'],
+      [true, false,  false, 'First'],
+      [false, true,   true, 'Last Company'],
+      [false, true,  false, 'Last'],
+      [false, false,  true, 'Company'],
+      [false, false, false, '']
+    ].each do |(first_name_presence, last_name_presence, company_name_presence, expected_result)|
+
+      it "fills the address with following arguments: {
+        (
+          #{first_name_presence},
+          #{last_name_presence},
+          #{company_name_presence}
+        )
+      }" do
+        address = build(:address)
+        address.first_name = first_name_presence     ? first_name   : nil
+        address.last_name = last_name_presence       ? last_name    : nil
+        address.company_name = company_name_presence ? company_name : nil
+
+        expect(address.full_name).to eq expected_result
       end
     end
   end
