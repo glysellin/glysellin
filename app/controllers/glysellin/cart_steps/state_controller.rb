6d0c6ea1115df30e701be76f4fe8f67188e1dd69
vala <gonjo@free.fr>
2014-08-22 17:11:47 +0200

    cancel order and restore cart when accessing previous state from the complete step

--- app/controllers/glysellin/cart_steps/state_controller.rb
+++ app/controllers/glysellin/cart_steps/state_controller.rb
@@ -5,6 +5,10 @@ module Glysellin
         state = params[:state]
 
         if current_cart.available_states.include?(state)
+          if current_cart.order
+            current_cart.cancel_order!
+          end
+
           current_cart.update_attributes(state: state)
         end
 
