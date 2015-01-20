fd2ea93471124a0420af6fe4f116cd2fdaf5ff2f
zenati <zenati@users.noreply.github.com>
2015-01-20 17:31:18 +0100

    We don't need barcodes

--- app/models/glysellin/variant.rb
+++ app/models/glysellin/variant.rb
@@ -32,8 +32,7 @@ module Glysellin
     # validate :check_properties
     before_validation :check_prices
     before_validation :ensure_name
-
-    validate :generate_barcode, on: :create, unless: Proc.new { |variant| variant.sku.present? }
+    
     # validates_numericality_of :eot_price, :price
     validates :name, presence: true
 
