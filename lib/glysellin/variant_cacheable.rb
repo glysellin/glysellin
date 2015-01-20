6c5f9458dae70bc6f27ed3d80c3872c228fcf982
zenati <zenati@users.noreply.github.com>
2015-01-20 17:40:38 +0100

    Removed :refresh_long_name method and hook

--- lib/glysellin/variant_cacheable.rb
+++ lib/glysellin/variant_cacheable.rb
@@ -3,7 +3,7 @@ module Glysellin
     extend ActiveSupport::Concern
 
     included do
-      after_save :refresh_variants_long_name
+      
     end
 
     def variants(*args)
@@ -11,18 +11,6 @@ module Glysellin
             "the #variants method which would return an " \
             "ActiveRecord::Relation containing the variants whose long_name " \
             "should be refreshed."
-    end
-
-    def refresh_variants_long_name
-      refreshable_variants = variants.includes(
-        variant_properties: :property,
-        sellable: { taxonomy: { parent: :parent } }
-      )
-
-      refreshable_variants.each do |variant|
-        variant.refresh_long_name
-        variant.save(validate: false)
-      end
     end
   end
 end
