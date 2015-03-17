3352804cafb9559a7729c2ef3ea48f3e8c691db8
zenati <zenati@users.noreply.github.com>
2015-03-17 15:24:15 +0100

    Adds glysellin_ prefix

--- db/migrate/20140619142436_create_taxonomies_store_clients.rb
+++ db/migrate/20140619142436_create_taxonomies_store_clients.rb
@@ -1,6 +1,6 @@
 class CreateTaxonomiesStoreClients < ActiveRecord::Migration
   def change
-    create_table :taxonomies_store_clients do |t|
+    create_table :glysellin_taxonomies_store_clients do |t|
       t.references :taxonomy
       t.references :store_client
       t.timestamps
