4a329733d56b2b432760f2619922df35d1ed5da0
vala <gonjo@free.fr>
2015-05-11 17:21:31 +0200

    cache order totals for vat rates processing to go from O(N*N) to O(N), reducing from seconds to milliseconds their processing time in most situations

--- lib/glysellin/vat_rates.rb
+++ lib/glysellin/vat_rates.rb
@@ -4,9 +4,18 @@ module Glysellin
 
     def initialize order
       @order = order
+      refresh
+    end
+
+    def refresh
+      @order_total_eot_price = order.total_eot_price
+      @order_eot_subtotal = order.eot_subtotal
+      @order_discounts_total_vat = order.discounts_total_vat
 
       process_items_rates!
       process_shipment_rates!
+
+      self
     end
 
     private
@@ -23,7 +32,7 @@ module Glysellin
       end
     end
 
-    def add rate, value
+    def add(rate, value)
       rate = rate.round(2)
       self[rate] ||= 0
       self[rate] += value
@@ -31,9 +40,9 @@ module Glysellin
 
     def item_total_vat item
       # share of the order total price before discounts are applied
-      weight = item.total_eot_price / order.eot_subtotal
+      weight = @order_total_eot_price / @order_eot_subtotal
       # total vat price deducing its share of the discounts vat
-      item.total_vat + (weight * order.discounts_total_vat)
+      item.total_vat + (weight * @order_discounts_total_vat)
     end
   end
 end
