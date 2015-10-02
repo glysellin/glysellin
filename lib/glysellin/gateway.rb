957557db8e279c19b1b22d3063796444ff320da2
Gabriel Cardoso <gcardoso.w@gmail.com>
2015-10-02 15:28:20 +0200

    rescue system pay requirement as it's located on github

--- lib/glysellin/gateway.rb
+++ lib/glysellin/gateway.rb
@@ -3,8 +3,14 @@ require 'glysellin/gateway/paypal_integral'
 require 'glysellin/gateway/atos'
 require 'glysellin/gateway/cic'
 require 'glysellin/gateway/check'
-require 'glysellin/gateway/systempay'
-require 'system_pay/railtie'
+
+# system_pay is a Gem (located on Github)[github.com/youboox/system_pay]
+# and we cannot depend on it. We should fork it and push it on Rubygems
+begin
+  require 'system_pay/railtie'
+  require 'glysellin/gateway/systempay'
+rescue
+end
 
 module Glysellin
   module Gateway
